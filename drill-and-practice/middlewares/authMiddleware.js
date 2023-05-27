acee3339088030efa44ff2f492d5da5b2e1898a7
Antti-Jussi Ikonen <ikonenaj@gmail.com>
2023-05-28 00:37:13 +0300

    Modified authMiddleware

--- drill-and-practice/middlewares/authMiddleware.js
+++ drill-and-practice/middlewares/authMiddleware.js
@@ -1,9 +1,13 @@
+const restrictedPaths = ["/topics", "/quiz"];
+
 const authMiddleware = async (context, next) => {
     const {state, request, response} = context;
     const user = await state.session.get("user");
     const path = request.url.pathname;
-    if (!user && (path.startsWith("/topic") || path.startsWith("/quiz"))) {
-        response.redirect("/auth/register");
+    if (!user && restrictedPaths.some(path => 
+        request.url.pathname.startsWith(path))
+    ) {
+        response.redirect("/auth/login");
     } else {
         context.user = user;
         await next();
